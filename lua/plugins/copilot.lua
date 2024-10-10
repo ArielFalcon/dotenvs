8f10eadd3fc44aaf00ff2afaafa9db41066296f4
Ariel Falcón <ariel.falcon@zenerconsulting.com>
2024-10-10 19:45:04 +0200

    refactored

--- lua/plugins/copilot.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "github/copilot.vim",
-   config = function()
-    vim.g.copilot_assume_mapped = true
-    vim.g.copilot_no_tab_map = true  -- Evitar que Copilot use la tecla <Tab>
-  end,  
-}
--- lua/vim-options.lua
+++
@@ -1,9 +0,0 @@
-vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.g.mapleader = " "
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.o.swapfile = false
-vim.opt.termguicolors = true
