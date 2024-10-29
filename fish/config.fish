f5626548bcd79909beb00e76b48990c48e1053b1
Ariel Falcón <ariel.falcon@zenerconsulting.com>
2024-10-29 12:42:32 +0100

    +configuracion de zsh

--- fish/config.fish
+++ fish/config.fish
@@ -23,8 +23,6 @@ end
 #end
 
 
-starship init fish | source
-
 set -x LS_COLORS "di=38;5;73:ow=48;5;22:ex=38;5;131:ln=38;5;179:*.tar=38;5;209:*.zip=38;5;209:*.jpg=38;5;176:*.png=38;5;176:*.mp3=38;5;176:*.wav=38;5;176:*.txt=38;5;223:*.sh=38;5;131"
 
 ## alias
