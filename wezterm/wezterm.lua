d6927b98cef95a5d9ebb17bf64ddf7d3b56fd9f6
Ariel Falcón <ariel.falcon@zenerconsulting.com>
2024-12-15 19:58:19 +0100

    zellij config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -34,12 +34,10 @@ config.window_padding = {
 	left = 0,
 	right = 0,
 	top = 0,
-	bottom = 0,
 }
 
 -- This is where you actually apply your config choices
 config.window_decorations = "RESIZE" -- removes window title bar
-config.force_reverse_video_cursor = true
 config.colors = {}
 config.colors.foreground = "#ebdbb2"
 config.colors.background = "#282828"
